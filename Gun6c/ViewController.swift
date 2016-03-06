560d78b75d0e54e1b815f0d992ecf1a02b2a10c7
Cenk Çokiçen <cenkc@windowslive.com>
2016-03-06 13:38:32 +0200

    Initial Commit

---
+++ Gun6c/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gun6c
+//
+//  Created by Cenk Çokiçen on 06/03/16.
+//  Copyright © 2016 Cenk Çokiçen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
